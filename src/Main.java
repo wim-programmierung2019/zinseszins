c6fb82fd82cc470b88803e46cd70f9872dfc2a2e
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-18 15:49:37 +0000

    Zinseszins Stub

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    /**
+     * Berechnung des Zinseszins
+     */
+    public static void main(String[] args){
+        // Berechnen Sie den Zinseszins für einen festgelegten 
+        // Zinssatz, Ausgangswert und Zeitraum. 
+        // Geben Sie die Zwischenwerte für jedes Jahr auf der Konsole aus.
+                
+    }
+}
