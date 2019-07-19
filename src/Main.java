c4f13314bc0a06a1aabbb289066858836de85ce0
BendingUnit29 <jbharbers@t-online.de>
2019-07-19 14:17:25 +0000

    Zinseszins Stub

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,17 @@ public class Main {
      * Berechnung des Zinseszins
      */
     public static void main(String[] args){
+        double zins = -0.0037;
+        double kapital = 100000.00;
+        int zeitraumInJahr = 5;
+        for(int i=1;i<=zeitraumInJahr;i++)
+            {
+                double print = (kapital*zins);
+                kapital = (kapital+(kapital*zins));
+                System.out.println("Zinsertrag: " + print);
+                System.out.println("Kapitalbetrag: " + kapital);
+                System.out.println("Zinsjahr: " + i);
+            }        
         // Berechnen Sie den Zinseszins für einen festgelegten 
         // Zinssatz, Ausgangswert und Zeitraum. 
         // Geben Sie die Zwischenwerte für jedes Jahr auf der Konsole aus.
