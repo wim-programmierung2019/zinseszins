44d0dfe8957c79107a0f984ad03b5454795713bf
ronjalinnenbrink <ronja.linnenbrink@claas.com>
2019-07-19 14:29:24 +0000

    keine

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,15 @@ public class Main {
         // Berechnen Sie den Zinseszins für einen festgelegten 
         // Zinssatz, Ausgangswert und Zeitraum. 
         // Geben Sie die Zwischenwerte für jedes Jahr auf der Konsole aus.
-                
+        double Zinssatz = 1.02;
+        double Ausgangswert= 100;
+        double Zeitraum = 5;
+        double Zwischenwert;
+        
+        for(int i=1; i<=Zeitraum; i++){
+            Zwischenwert = Ausgangswert * Zinssatz;
+            System.out.println("Jahr "+ i + "Zwischenwert " + Zwischenwert);
+            Ausgangswert = Zwischenwert;     
+            }
+          }
     }
-}
