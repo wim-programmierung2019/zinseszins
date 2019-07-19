f4b4fd79b47a75b67980d8014c5df899210c7a41
Matthias Vaagt <matthias.vaagt@gmail.com>
2019-07-19 14:32:06 +0000

    Matthias Schleife

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,19 @@ public class Main {
      */
     public static void main(String[] args){
         // Berechnen Sie den Zinseszins für einen festgelegten 
-        // Zinssatz, Ausgangswert und Zeitraum. 
-        // Geben Sie die Zwischenwerte für jedes Jahr auf der Konsole aus.
-                
-    }
-}
+        double zinssatz = 1.05;
+        double kapital = 1000;
+        System.out.println("Zinssatz="+zinssatz+"%");
+        System.out.println("Kapital="+kapital+"€");
+        double zeitraum=5; 
+        double zinsen=kapital*zinssatz; 
+        
+        for (int i=1; i<=zeitraum;i++)
+        {
+            kapital=kapital*zinssatz;
+            System.out.println("Jahr"+i+": "+kapital);
+            
+            
+            }}}   
+
+
