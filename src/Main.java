49f64df2ee57e56b55b306fc7938f284a8b1128f
jonassmolarek <jonas.smolarek@agravis.de>
2019-07-19 14:31:33 +0000

    Lösung Aufgabe

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner; 
 public class Main {
     
     /**
@@ -7,6 +8,25 @@ public class Main {
         // Berechnen Sie den Zinseszins für einen festgelegten 
         // Zinssatz, Ausgangswert und Zeitraum. 
         // Geben Sie die Zwischenwerte für jedes Jahr auf der Konsole aus.
-                
+
+        double zinssatz;
+        double Ausgangswert;
+        int Zeitraum;
+
+        Ausgangswert= 1000.00; 
+        Zeitraum = 5;
+        zinssatz = 0.05;
+        
+        for (int i= 1; i <= Zeitraum; i++){
+            double neuerwert = Ausgangswert * zinssatz;
+            //Ausgabe
+            System.out.println("Zeitpunkt: " + i);
+            System.out.println("Ausgangswert: " + Ausgangswert);
+            System.out.println("Zinssatz: " + zinssatz);
+            System.out.println("Neuer Wert: " + neuerwert);
+            Ausgangswert = neuerwert;
+            
+        }
+    
     }
 }
