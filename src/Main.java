7dc090dcdba48097d42f873fac978e93c7fc9ab4
rybalt <arybaltschenko@web.de>
2019-07-19 14:27:38 +0000

    Erstellung

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,17 @@ public class Main {
      * Berechnung des Zinseszins
      */
     public static void main(String[] args){
-        // Berechnen Sie den Zinseszins für einen festgelegten 
+        // Berechnen Sie den Zinseszins für einen festgelegten
         // Zinssatz, Ausgangswert und Zeitraum. 
         // Geben Sie die Zwischenwerte für jedes Jahr auf der Konsole aus.
+        
+        double Zinssatz = 1.1;
+        double Ausgangswert = 1000;
+        int Zeitraum = 10;
                 
+        for(int i=1; i<Zeitraum; i++){
+            Ausgangswert = Ausgangswert*Zinssatz;
+            System.out.println("Jahr" + i + "Der Ausgangswert ist: " + Ausgangswert);
+        }
     }
 }
