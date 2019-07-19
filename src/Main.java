2f21ce1c53313f6d3e196b44501c1d5b52f3a687
JSteinhoff1 <jannik.steinhoff@gmx.de>
2019-07-19 14:14:27 +0000

    Formel angelegt

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,13 @@ public class Main {
         // Berechnen Sie den Zinseszins für einen festgelegten 
         // Zinssatz, Ausgangswert und Zeitraum. 
         // Geben Sie die Zwischenwerte für jedes Jahr auf der Konsole aus.
+        double zinssatz = 0.05;
+        int zeitraum = 10;
+        double ausgangswert = 1000;
+        
+        for(int i=1; i<zeitraum; i++){
+            System.out.println(ausgangswert=ausgangswert*(1+zinssatz));
+        }
                 
     }
 }
